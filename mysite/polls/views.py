f7692387ddd34f01c5c98e90d7563359378baa0a
Patiphan Srichai <patiphansrichai@Patiphans-MacBook-Pro.local>
2020-09-12 11:02:35 +0700

    Can view text from function in view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You are at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're look at question %s." %question_id)
+
+def results(request, question_id):
+    response = "Yore're look at the result of questoin %s."
+    return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
