1490169ff51294f6b8d8dfc86e3bd984881b7167
Patiphan Srichai <patiphansrichai@Patiphans-MacBook-Pro.local>
2020-10-25 10:24:27 +0700

    Add working register login logout page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,12 +32,14 @@ DATABASES = {
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
